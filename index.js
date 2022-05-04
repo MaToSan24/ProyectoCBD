52b52d9b19c76caad0f3949a38407735970cdc0d
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2022-05-04 15:05:06 +0200

    Data parsed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const res = require('express/lib/response');
 const OrientDBClient = require("orientjs").OrientDBClient;
 
 async function conectarBD() {
@@ -17,13 +18,132 @@ async function conectarBD() {
   // Aquí se abre la sesion
   
   try {
-    // let result = await session.query("select from OUser where name = :name", {params: { name: "admin" }})
-    let result = await session.query("SELECT name, apellido FROM #11:0")
-    // let result = await session.command("CREATE VERTEX V SET name='Antonio', apellido='Biñuela'")
-      .all();
-    console.log(result);
+    // // let result = await session.query("select from OUser where name = :name", {params: { name: "admin" }})
+    // let result = await session.query("SELECT name, apellido FROM #11:0")
+    // // let result = await session.command("CREATE VERTEX V SET name='Antonio', apellido='Biñuela'")
+    //   .all();
+    // console.log(result);
+
+    const fs = require('fs');
+    
+    // ARTIST
+    
+    let file = fs.readFileSync(path.join(__dirname, 'Dataset LastFM/artists.dat'), 'utf8')
+    await session.command(`DROP CLASS Artist UNSAFE`)
+    await session.command(`CREATE CLASS Artist EXTENDS V`)
+  
+    for (let [index, line] of file.split("\n").entries()) {
+      if (index !== 0) {
+        let lineSplit = line.split("\t")
+        let id = lineSplit[0]
+        let name = lineSplit[1].replace(/\"+/gm, "'").replace(/\\+/gm, "\\\\")
+        let url = lineSplit[2].replace(/\"+/gm, "'").replace(/\\+/gm, "\\\\")
+        let pictureURL = lineSplit[3].replace(/\"+/gm, "'").replace(/\\+/gm, "\\\\")
+
+        console.log(`Line: ${id}, ${name}, ${url}, ${pictureURL}`)
+        let result = await session.command(`INSERT INTO Artist SET id="${id}", name="${name}", url="${url}", pictureURL="${pictureURL}"`).all()
+      }
+    }
+
+    console.log("Artists inserted")
+
+    // USER
+    
+    file = fs.readFileSync(path.join(__dirname, 'Dataset LastFM/users.dat'), 'utf8')
+    await session.command(`DROP CLASS User UNSAFE`)
+    await session.command(`CREATE CLASS User EXTENDS V`)
+  
+    for (let [index, line] of file.split("\r\n").entries()) {
+      if (index !== 0) {
+        let id = line
+
+        console.log(`Line: ${id}`)
+        let result = await session.command(`INSERT INTO User SET id="${id}"`).all()
+      }
+    }
+
+    console.log("Users inserted")
+
+    // USER_FRIENDS
+    
+    file = fs.readFileSync(path.join(__dirname, 'Dataset LastFM/user_friends.dat'), 'utf8')
+    await session.command(`DROP CLASS Friend UNSAFE`)
+    await session.command(`CREATE CLASS Friend EXTENDS E`)
+  
+    for (let [index, line] of file.split("\r\n").entries()) {
+      if (index !== 0) {
+        let lineSplit = line.split("\t")
+        let userId = lineSplit[0]
+        let friendId = lineSplit[1]
+
+        console.log(`Line: ${userId}, ${friendId}`)
+        let result = await session.command(`CREATE EDGE Friend FROM (SELECT FROM User WHERE id="${userId}") TO (SELECT FROM User WHERE id="${friendId}")`).all()
+      }
+    }
+    
+    console.log("User's friendship inserted")
+
+    // USER_ARTISTS
+    
+    file = fs.readFileSync(path.join(__dirname, 'Dataset LastFM/user_artists.dat'), 'utf8')
+    await session.command(`DROP CLASS Listen UNSAFE`)
+    await session.command(`CREATE CLASS Listen EXTENDS E`)
+  
+    for (let [index, line] of file.split("\r\n").entries()) {
+      if (index !== 0) {
+        let lineSplit = line.split("\t")
+        let userId = lineSplit[0]
+        let artistId = lineSplit[1]
+        let listeningCount = lineSplit[2]
+
+        console.log(`Line: ${userId}, ${artistId}, ${listeningCount}`)
+        let result = await session.command(`CREATE EDGE Listen FROM (SELECT FROM User WHERE id="${userId}") TO (SELECT FROM Artist WHERE id="${artistId}") SET listeningCount=${listeningCount}`).all()
+      }
+    }
+    
+    console.log("User's artists inserted")
+
+    // TAGS
+    
+    file = fs.readFileSync(path.join(__dirname, 'Dataset LastFM/tags.dat'), 'utf8')
+    await session.command(`DROP CLASS Tag UNSAFE`)
+    await session.command(`CREATE CLASS Tag EXTENDS V`)
+  
+    for (let [index, line] of file.split("\r\n").entries()) {
+      if (index !== 0) {
+        let lineSplit = line.split("\t")
+        let id = lineSplit[0]
+        let value = lineSplit[1]
+
+        console.log(`Line: ${id}, ${value}`)
+        let result = await session.command(`INSERT INTO Tag SET id="${id}", value="${value}"`).all()
+      }
+    }
+
+    console.log("Tags inserted")
+
+    // TAGGED_ARTISTS
+    
+    file = fs.readFileSync(path.join(__dirname, 'Dataset LastFM/taggedartists.dat'), 'utf8')
+    await session.command(`DROP CLASS Tagged UNSAFE`)
+    await session.command(`CREATE CLASS Tagged EXTENDS E`)
+  
+    for (let [index, line] of file.split("\r\n").entries()) {
+      if (index !== 0) {
+        let lineSplit = line.split("\t")
+        let artistId = lineSplit[0]
+        let tagId = lineSplit[1]
+
+        console.log(`Line: ${artistId}, ${tagId}`)
+        let result = await session.command(`CREATE EDGE Tagged FROM (SELECT FROM Artist WHERE id="${artistId}") TO (SELECT FROM Tag WHERE id="${tagId}")`).all()
+      }
+    }
+    
+    console.log("Artists' tags inserted")
+
+
   } catch (err) {
-    console.log(err);
+    console.log("Error: " + err);
   }
   
   // Aquí se cierra la sesión
